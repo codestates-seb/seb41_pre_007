cd10378dbe9af51496b936e6a6875f89f077153e
keumbi <rmaqldi@gmail.com>
2022-12-23 02:04:53 +0900

    ✨ feat: answer 구현 수정

--- server/src/main/java/com/pre007/server/answer/controller/AnswerController.java
+++ server/src/main/java/com/pre007/server/answer/controller/AnswerController.java
@@ -6,14 +6,11 @@ import com.pre007.server.answer.mapper.AnswerMapper;
 import com.pre007.server.answer.service.AnswerService;
 import com.pre007.server.dtoUtils.MultiResponseDto;
 import com.pre007.server.dtoUtils.SingleResponseDto;
-import com.pre007.server.member.entity.Member;
-import com.pre007.server.member.service.MemberService;
-import com.pre007.server.question.entity.Question;
-import com.pre007.server.question.service.QuestionService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,66 +19,60 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/answers")
-@Valid
+@Validated
 @AllArgsConstructor
 public class AnswerController {
     private final AnswerService answerService;
     private final AnswerMapper mapper;
-    private final MemberService memberService;
-    private final QuestionService questionService;
 
 
     @PostMapping
-    public ResponseEntity postAnswer(@Valid @RequestBody AnswerDto.post requestBody) {
-         Member finndMember = memberService.findMember(requestBody.getMemberId());
-         Question findQuestion = questionService.findQuestion(requestBody.getQuestionId());
-
-        Answer answer = mapper.answerPostDtoToAnswer(requestBody);
-         answer.setMember(finndMember);
-         answer.setQuestion(findQuestion);
-
-        Answer createAnswer = answerService.createAnswer(answer);
-        AnswerDto.Response response = mapper.an(createAnswer);
-
+    public ResponseEntity postAnswer(@Valid @RequestBody AnswerDto.Post postRequest) {
+//
+//        Answer answer = mapper.answerPostDtoToAnswer(postRequest);
+        Answer answer = answerService.createAnswer(mapper.answerPostDtoToAnswer(postRequest));
 
         return new ResponseEntity<>(
-                new SingleResponseDto<>(response),
-                HttpStatus.CREATED
-        );
+                new SingleResponseDto<>(mapper.answerToAnswerResponse(answer)),
+                HttpStatus.CREATED);
     }
 
-    @PatchMapping("/edit/{answer-id}")
+    @PatchMapping("/{answer-id}")
     public ResponseEntity patchAnswer(@PathVariable("answer-id") @Positive long answerId,
-                                     @Valid @RequestBody AnswerDto.Patch requestBody) {
-        requestBody.setAnswerId(answerId);
-        Answer answer = answerService.updateAnswer(mapper.answerPatchDtoToAnswer(requestBody));
+                                     @Valid @RequestBody AnswerDto.Patch patchRequest) {
+        patchRequest.setAnswerId(answerId);
+        Answer answer = mapper.answerPatchDtoToAnswer(patchRequest);
+        Answer updatedAnswer = answerService.updateAnswer(answer);
 
-        return new ResponseEntity<>(new SingleResponseDto<>(mapper.answerToAnswerResponseDto(answer)), HttpStatus.OK);
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(mapper.answerToAnswerResponse(answer)), HttpStatus.OK);
     }
 
     @GetMapping("/{answer-id}")
-    public ResponseEntity getAnswer(@PathVariable("answer-id") @Positive long answerId) {
-        Answer answer = answerService.findVerifiedAnswer(answerId);
+    public ResponseEntity getAnswer(@PathVariable("answer-id") long answerId) {
+        Answer answer = answerService.findAnswer(answerId);
 
         return new ResponseEntity<>(
-                new SingleResponseDto<>(mapper.answerToAnswerResponseDto(answer)), HttpStatus.OK);
+                new SingleResponseDto<>(mapper.answerToAnswerResponse(answer)), HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity getAnswers(@Positive @RequestParam int page,
-                                     @Positive @RequestParam int size) {
+    public ResponseEntity getFindAnswers(@Positive @RequestParam int page,
+                                         @Positive @RequestParam int size) {
         Page<Answer> pageAnswers = answerService.findAnswers(page - 1, size);
-        List<Answer> answers = pageAnswers.getContent();
+        List<Answer> answerListForResponse = pageAnswers.getContent();
+        List<AnswerDto.Response> responses = mapper.answerListToAnswerResponsesList(answerListForResponse);
 
-        return new ResponseEntity<>(
-                new MultiResponseDto<>(mapper.answersToAnswerResponsesDtos(answers), pageAnswers), HttpStatus.OK);
+        return new ResponseEntity<>(new MultiResponseDto<>(responses, pageAnswers), HttpStatus.OK);
     }
 
     @DeleteMapping("/{answer-id}")
     public ResponseEntity deleteAnswer(@PathVariable("answer-id") long answerId) {
         answerService.deleteAnswer(answerId);
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
 
 }
 
--- server/src/main/java/com/pre007/server/answer/dto/AnswerDto.java
+++ server/src/main/java/com/pre007/server/answer/dto/AnswerDto.java
@@ -1,54 +1,52 @@
 package com.pre007.server.answer.dto;
 
+import com.pre007.server.answer.entity.Answer;
 import com.pre007.server.audit.Auditable;
 import com.pre007.server.comment.entity.Comment;
 import com.pre007.server.vote.entity.Vote;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 
 public class AnswerDto {
     @AllArgsConstructor
     @Getter
-    @NoArgsConstructor
-    public class post {
-
-
+    public static class Post {
         @NotBlank(message = "답변을 입력해주세요.")
+        // @Range(max= 500)
         private String content;
+        @Positive
+        @NotNull
         private Long questionId;
         private Long memberId;
     }
 
     @AllArgsConstructor
     @Getter
-    public class Patch {
+    public static class Patch {
         private Long answerId;
-        @NotBlank(message = "답변을 입력해주세요.")
         private String content;
+        private Answer.AnswerStatus answerStatus;
 
         public void setAnswerId(long answerId) {
             this.answerId = answerId;
         }
-
     }
 
     @AllArgsConstructor
     @Getter
-    @Setter
-    public class Response extends Auditable {
-        private long answerId;
-
+    public static class Response extends Auditable {
+        private Long answerId;
+        private Long memberId;
         private String content;
 
-//        private LocalDateTime createdAt;
-//
-//        private LocalDateTime modifiedAt;
-
-        private List<Vote> votes;
+        private List<Vote> votes; // 갯수만 가져오는가? int?
         private List<Comment> comments;
+        private Answer.AnswerStatus answerStatus;
 
     }
 }
