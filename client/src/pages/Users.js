2899122e84dd546d24575a94e1094f1021f1c7fd
SUBINSON <fraisecheris@gmail.com>
2022-12-23 16:36:15 +0900

    ✨ feat: Users Page's structure

---
+++ client/src/pages/Users.js
@@ -0,0 +1,102 @@
+import { Sidebar } from '../components/Sidebar';
+import styled from 'styled-components';
+import { SidebarRight } from '../components/SidebarRight';
+
+const SWrapper = styled.div`
+  display: flex;
+`;
+
+const SUsers = styled.main`
+  display: flex;
+  flex-direction: column;
+  max-width: 1100px;
+  width: 1100px;
+  border: 1px solid #ececec;
+  border-width: 0 0 0 1px;
+
+  .users-content-container {
+    max-width: 800px;
+    margin: 0 0 3em;
+    margin-block-start: 0.67em;
+    margin-block-end: 0.67em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    margin-left: 5%;
+  }
+
+  .users-title {
+    display: block;
+    font: inherit;
+    font-size: 2rem;
+  }
+
+  .users-filter-container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .users-filter-button {
+    background-color: white;
+    border: 0.5px solid gray;
+    border-radius: 5px;
+    :hover {
+      background-color: #ececec;
+    }
+  }
+
+  .users-period-container {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .users-period-button {
+    border: none;
+    background-color: transparent;
+    color: #6a737c;
+    :hover,
+    :active {
+      color: #3b4045;
+      border-bottom: 1px solid #f48024;
+    }
+    :active {
+      font-weight: bold;
+    }
+  }
+`;
+
+const Users = () => {
+  return (
+    <SWrapper>
+      <Sidebar />
+      <SUsers>
+        <div className="users-content-container">
+          <div className="users-title-container">
+            <h1 className="users-title">Users</h1>
+          </div>
+          <div className="users-filter-container">
+            <form className="users-filter-search">
+              <input type="search"></input>
+            </form>
+            <div className="users-filter-button-container">
+              <button className="users-filter-button">Reputation</button>
+              <button className="users-filter-button">New users</button>
+              <button className="users-filter-button">Voters</button>
+              <button className="users-filter-button">Editors</button>
+              <button className="users-filter-button">Moderators</button>
+            </div>
+          </div>
+          <div className="users-period-container">
+            <button className="users-period-button">week</button>
+            <button className="users-period-button">month</button>
+            <button className="users-period-button">quarter</button>
+            <button className="users-period-button">year</button>
+            <button className="users-period-button">all</button>
+          </div>
+        </div>
+      </SUsers>
+      <SidebarRight />
+    </SWrapper>
+  );
+};
+
+export default Users;
