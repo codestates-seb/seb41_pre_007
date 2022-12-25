1a90955212a260e009bf9ed51b315f206f83f08c
SUBINSON <fraisecheris@gmail.com>
2022-12-25 14:24:38 +0900

    ✨ feat: User Profile Page's structure

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import { QuestionPost } from './pages/QuestionPost';
 import Footer from './components/Footer';
 import LoginHeader from './components/LoginHeader';
 import { AllQuestions } from './pages/AllQuestions';
+import UsersProfile from './pages/UsersProfile';
 
 const App = () => {
   return (
     <BrowserRouter>
       <LoginHeader />
       <Routes>
+        <Route path="/users/profile" element={<UsersProfile />} />
         <Route path="/home" element={<Home />} />
         <Route path="/questionPost" element={<QuestionPost />} />
         <Route path="/allQuestions" element={<AllQuestions />} />
---
+++ client/src/pages/UsersProfile.js
@@ -0,0 +1,122 @@
+import styled from 'styled-components';
+import { Sidebar } from '../components/Sidebar';
+import Avatar from '../components/Avatar';
+import { ReactComponent as Cake } from '../image/Cake.svg';
+import { ReactComponent as Time } from '../image/Time.svg';
+import { ReactComponent as Location } from '../image/Location.svg';
+import { ReactComponent as Pencil } from '../image/Pencil.svg';
+import { ReactComponent as Network } from '../image/Network.svg';
+import dummyUsers from '../db/dummyUsers.json';
+
+const SWrapper = styled.div`
+  display: flex;
+`;
+
+const SProfile = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 1100px;
+  border: 1px solid #ececec;
+  border-width: 0 0 0 1px;
+
+  .profile-content-wrapper {
+    display: flex;
+    justify-content: space-between;
+    margin: 30px;
+  }
+
+  .profile-content-container {
+    display: flex;
+  }
+
+  .user-inform-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 15px;
+  }
+
+  .user-inform-nickname {
+    font-size: 32px;
+    font-weight: 500;
+    margin-bottom: 3px;
+  }
+
+  .user-detail-container {
+    display: flex;
+    align-items: center;
+    margin: 4px 0;
+  }
+
+  .color-gray {
+    color: hsl(210, 8%, 60%);
+  }
+
+  .font-size-12 {
+    font-size: 12px;
+  }
+
+  .user-detail {
+    margin: 0 5px;
+  }
+
+  .user-button {
+    background-color: transparent;
+    border: 1px solid hsl(210, 8%, 60%);
+    border-radius: 3px;
+    padding: 4px 8px;
+    margin: 0 3px;
+  }
+`;
+
+const UsersProfile = () => {
+  const recentUser = dummyUsers.users.filter((user) => user.id === 1);
+
+  return (
+    <SWrapper>
+      <Sidebar />
+      <SProfile>
+        <div className="profile-content-wrapper">
+          <div className="profile-content-container">
+            <div className="user-profile-avatar">
+              <Avatar image={recentUser[0].avatar} size="128" />
+            </div>
+            <div className="user-inform-container">
+              <div className="user-inform-nickname">
+                {recentUser[0].userNickname}
+              </div>
+              <div className="user-detail-container">
+                <Cake fill="hsl(210,8%,60%)" />
+                <div className="user-detail color-gray font-size-12">
+                  Member for 1months
+                </div>
+                <Time fill="hsl(210,8%,60%)" />
+                <div className="user-detail color-gray font-size-12">
+                  Last seen today
+                </div>
+              </div>
+              <div className="user-detail-container">
+                <Location fill="hsl(210,8%,60%)" />
+                <div className="user-detail color-gray font-size-12">
+                  {recentUser[0].userRegion}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="user-button-container">
+            <button className="user-button">
+              <Pencil fill="hsl(210,8%,60%)" />
+              <span className="color-gray font-size-12">Edit profile</span>
+            </button>
+            <button className="user-button">
+              <Network />
+              <span className="color-gray font-size-12">Network profile</span>
+            </button>
+          </div>
+        </div>
+      </SProfile>
+    </SWrapper>
+  );
+};
+
+export default UsersProfile;
