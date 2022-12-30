28956fdac62214f6376c6ace4d8e2e4c704ddc43
keumbi <rmaqldi@gmail.com>
2022-12-30 17:09:46 +0900

    🐛 fix: 에러 핸들링, 답변 내용 사라짐, 주석 제거

--- server/src/main/java/com/pre007/server/question/dto/QuestionDto.java
+++ server/src/main/java/com/pre007/server/question/dto/QuestionDto.java
@@ -32,8 +32,7 @@ public class QuestionDto {
     public static class Response {
         private Long questionId;
         private String title;
-        /*private String content;*/
-
+        private String content;
         private LocalDateTime createdAt;
         private LocalDateTime modifiedAt;
         private Long memberId;
