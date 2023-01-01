ad8a313aafe78489dd76010afe1b5425f25680ba
djwjfk <grw07236@naver.com>
2023-01-02 01:39:42 +0900

    🐛 fix: user 정보 불러오기

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -2,10 +2,10 @@ import { Sidebar } from '../components/Sidebar';
 import { ReactComponent as Search } from '../image/Search.svg';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-import dummyUsers from '../db/dummyUsers.json';
+import { useState, useEffect } from 'react';
+// import dummyUsers from '../db/dummyUsers.json';
 import Avatar from '../components/Avatar';
-// import axios from 'axios';
+import axios from 'axios';
 
 const SWrapper = styled.div`
   display: flex;
@@ -125,25 +125,28 @@ const SUsersContainer = styled.div`
 `;
 
 const Users = () => {
-  // const [items, setItem] = useState(null);
+  const newDate = new Date();
+  const today = `${newDate.toLocaleString()}`;
+  const [items, setItem] = useState([]);
   const navigate = useNavigate();
-  // const { page, size } = useParams();
+  const page = 1;
+  const size = 10;
 
-  // const userAxios = async () => {
-  //   axios
-  //     .get(`http://54.180.127.165:8080/members?page=1&size=10`)
-  //     .then((res) => {
-  //       setItem(res.data.data);
-  //       console.log('data:', res.data.data);
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //       console.log('에러!!!');
-  //     });
-  // };
-  // useEffect(() => {
-  //   userAxios();
-  // }, []);
+  const userAxios = async () => {
+    axios
+      .get(`http://54.180.127.165:8080/members?page=${page}&size=${size}`)
+      .then((res) => {
+        setItem(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        console.log('에러!!!');
+      });
+  };
+  useEffect(() => {
+    userAxios();
+  }, []);
 
   return (
     <SWrapper>
@@ -192,10 +195,10 @@ const Users = () => {
             <button className="users-period-button">all</button>
           </div>
           <SUsersContainer>
-            {dummyUsers.users.map((user) => (
-              <div className="users-inform-container" key={user.id}>
+            {items.map((user) => (
+              <div className="users-inform-container" key={user.memberId}>
                 <div className="users-inform-avatar">
-                  <Avatar image={user.avatar} size="48" />
+                  <Avatar image={user.profileImage} size="48" />
                 </div>
                 <div className="users-inform">
                   <div
@@ -203,19 +206,19 @@ const Users = () => {
                     role="presentation"
                     onClick={() => navigate('/users/:id')}
                   >
-                    {user.userNickname}
+                    {user.name}
                   </div>
-                  <div className="users-inform-region">{user.userRegion}</div>
+                  <div className="users-inform-region">{user.address}</div>
                   <div className="users-inform-reputation">
-                    {user.reputation}
+                    {user.createdAt && today}
                   </div>
-                  <div className="users-inform-tags-container">
+                  {/* <div className="users-inform-tags-container">
                     {user.tags.map((tag, idx) => (
                       <div key={idx} className="users-inform-tags">
                         {tag}
                       </div>
                     ))}
-                  </div>
+                  </div> */}
                 </div>
               </div>
             ))}
